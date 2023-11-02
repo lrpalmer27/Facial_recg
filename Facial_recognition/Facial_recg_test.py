53f5806a4fc601d4bab5ec1bbb85a95a66b958e2
lrpalmer27 <lrpalmer@mun.ca>
2023-11-02 17:48:07 -0230

    basics

---
+++ Facial_recognition/Facial_recg_test.py
@@ -0,0 +1,57 @@
+import mrcnn
+import mrcnn.config
+import mrcnn.model
+import mrcnn.visualize
+import cv2
+import os
+import random
+
+# load the class label names from disk, one label per line
+# CLASS_NAMES = open("coco_labels.txt").read().strip().split("\n")
+
+_TestDir="\\.PeopleData\\test\\"
+ROOT_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# CLASS_NAMES = ['BG', 'Person','bicycle', 'cell phone','book']
+CLASS_NAMES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+class SimpleConfig(mrcnn.config.Config):
+    # Give the configuration a recognizable name
+    NAME = "facialrec"
+    
+    # set the number of GPUs to use along with the number of images per GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+	# Number of classes = number of classes + 1 (+1 for the background). The background class is named BG
+    NUM_CLASSES = len(CLASS_NAMES)
+
+# Initialize the Mask R-CNN model for inference and then load the weights.
+# This step builds the Keras model architecture.
+model = mrcnn.model.MaskRCNN(mode="inference", 
+                             config=SimpleConfig(),
+                             model_dir=os.getcwd())
+
+# Load the weights into the model.
+model.load_weights(filepath=ROOT_DIR+"\\mask_rcnn_coco.h5", 
+                   by_name=True)
+
+# load the input image, convert it from BGR to RGB channel
+Test_img_list=os.listdir(ROOT_DIR+_TestDir) #lists the kangaroo test image dir
+randomImg=Test_img_list[random.randint(0,len(Test_img_list)-1)]
+image = cv2.imread(ROOT_DIR+_TestDir+randomImg) #picks a random image in the kangaroo test image dir.
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Perform a forward pass of the network to obtain the results
+r = model.detect([image], verbose=0)
+
+# Get the results for the first image.
+r = r[0]
+
+# Visualize the detected objects.
+print('Chosen random file to display with mask predictions: ',randomImg)
+mrcnn.visualize.display_instances(image=image, 
+                                  boxes=r['rois'], 
+                                  masks=r['masks'], 
+                                  class_ids=r['class_ids'], 
+                                  class_names=CLASS_NAMES, 
+                                  scores=r['scores'])
